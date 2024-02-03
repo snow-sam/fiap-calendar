e58bd2b8fa202409427345019ecf298a43c17088
samuel <samuel@convestconsultoria.com.br>
2024-02-03 02:13:52 +0000

    fix: remover pwa

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    swcMinify: true,      // Enable SWC minification for improved performance
-    compiler: {
-        removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
-    },
-}
+const nextConfig = {}
 
-const withPWA = require("next-pwa")({
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-});
-
-
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
